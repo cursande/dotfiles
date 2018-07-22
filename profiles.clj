9128e5f1209b8b0a394eec13e4274530cd424220
Alex F <adfarries@gmail.com>
2018-07-22 16:03:59 +1000

    Add profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,3 @@
+{:user {:dependencies [[pjstadig/humane-test-output "0.8.3"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}}
