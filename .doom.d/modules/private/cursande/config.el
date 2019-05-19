d427305070e53abbedcbb62da0534d0bc12d02f0
Alex F <adfarries@gmail.com>
2019-05-19 16:55:45 +1000

    Add hooks for rainbow-delimiters

--- .doom.d/modules/private/cursande/config.el
+++ .doom.d/modules/private/cursande/config.el
@@ -9,3 +9,15 @@
 (when (version<= "26.0.50" emacs-version)
   (global-display-line-numbers-mode))
 
+;; rainbow-delimiters added to individual major modes as it can
+;; cause problems if added globally https://github.com/Fanael/rainbow-delimiters
+(add-hook 'ruby-mode #'rainbow-delimiters-mode)
+(add-hook 'python-mode #'rainbow-delimiters-mode)
+(add-hook 'clojure-mode #'rainbow-delimiters-mode)
+(add-hook 'elixir-mode #'rainbow-delimiters-mode)
+(add-hook 'javascript-mode #'rainbow-delimiters-mode)
+(add-hook 'js2-mode #'rainbow-delimiters-mode)
+(add-hook 'cc-mode #'rainbow-delimiters-mode)
+
+;; For emacs 24 and above:
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
