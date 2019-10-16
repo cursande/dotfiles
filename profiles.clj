179dc016fbb42cdff39db069f234e652d63aa1f4
Alex F <adfarries@gmail.com>
2019-10-16 13:34:46 +1100

    Add yagni and kibit as plugins

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,5 @@
 {:user {:dependencies [[pjstadig/humane-test-output "0.8.3"]]
         :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}}
+                     (pjstadig.humane-test-output/activate!)]
+        :plugins [[venantius/yagni "0.1.7"]
+                  [lein-kibit "0.1.7"]]}}
